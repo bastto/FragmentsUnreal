2f2ab4f4d50389fde8c2e2fb16fcbd8da1b48345
Jairo Basto Picott <jairobpicott@gmail.com>
2025-07-15 19:10:09 +0100

    Add FragmentsEditor module with editor subsystem
    Introduces the FragmentsEditor Unreal Engine module, including build configuration, module interface, and the FragmentsImporterEditorSubsystem for fragment import and management in the editor. Updates the plugin descriptor to register the new editor module.

---
+++ Source/FragmentsEditor/Private/EditorSubsystems/FragmentsImporterEditorSubsystem.cpp
@@ -0,0 +1,101 @@
+
+
+
+#include "EditorSubsystems/FragmentsImporterEditorSubsystem.h"
+#include "Importer/FragmentsImporter.h"
+
+FString UFragmentsImporterEditorSubsystem::LoadFragment(const FString& FragPath)
+{
+    check(Importer);
+
+    FString ModelGuid = Importer->LoadFragment(FragPath);
+    FragmentModels = Importer->GetFragmentModels();
+
+    return ModelGuid;
+}
+
+void UFragmentsImporterEditorSubsystem::UnloadFragment(const FString& ModelGuid)
+{
+    if (Importer)
+        Importer->UnloadFragment(ModelGuid);
+
+    if (UFragmentModelWrapper** Found = FragmentModels.Find(ModelGuid))
+    {
+        (*Found) = nullptr;
+        FragmentModels.Remove(ModelGuid);
+    }
+}
+
+FString UFragmentsImporterEditorSubsystem::ProcessFragment(AActor* OwnerActor, const FString& FragPath, TArray<class AFragment*>& OutFragments, bool bSaveMeshes, bool bUseDynamicMesh)
+{
+    check(Importer);
+
+    FString ModelGuid = Importer->Process(OwnerActor, FragPath, OutFragments, bSaveMeshes, bUseDynamicMesh);
+
+    FragmentModels = Importer->GetFragmentModels();
+
+    return ModelGuid;
+}
+
+void UFragmentsImporterEditorSubsystem::ProcessLoadedFragment(const FString& InModelGuid, AActor* InOwnerRef, bool bInSaveMesh, bool bUseDynamicMesh)
+{
+    check(Importer);
+
+    Importer->ProcessLoadedFragment(InModelGuid, InOwnerRef, bInSaveMesh, bUseDynamicMesh);
+}
+
+void UFragmentsImporterEditorSubsystem::ProcessLoadedFragmentItem(int32 InLocalId, const FString& InModelGuid, AActor* InOwnerRef, bool bInSaveMesh, bool bUseDynamicMesh)
+{
+    check(Importer);
+
+    return Importer->ProcessLoadedFragmentItem(InLocalId, InModelGuid, InOwnerRef, bInSaveMesh, bUseDynamicMesh);
+}
+
+TArray<int32> UFragmentsImporterEditorSubsystem::GetElementsByCategory(const FString& InCategory, const FString& ModelGuid)
+{
+    check(Importer);
+
+    return Importer->GetElementsByCategory(InCategory, ModelGuid);
+}
+
+AFragment* UFragmentsImporterEditorSubsystem::GetItemByLocalId(int32 InLocalId, const FString& InModelGuid)
+{
+    check(Importer)
+
+        return Importer->GetItemByLocalId(InLocalId, InModelGuid);
+}
+
+FFragmentItem* UFragmentsImporterEditorSubsystem::GetFragmentItemByLocalId(int32 InLocalId, const FString& InModelGuid)
+{
+    check(Importer);
+    return Importer->GetFragmentItemByLocalId(InLocalId, InModelGuid);
+}
+
+void UFragmentsImporterEditorSubsystem::GetItemData(FFragmentItem* InFragmentItem)
+{
+    check(Importer);
+    Importer->GetItemData(InFragmentItem);
+}
+
+AFragment* UFragmentsImporterEditorSubsystem::GetModelFragment(const FString& InModelGuid)
+{
+    check(Importer);
+    return Importer->GetModelFragment(InModelGuid);
+}
+
+void UFragmentsImporterEditorSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+
+    Importer = NewObject<UFragmentsImporter>(this);
+}
+
+void UFragmentsImporterEditorSubsystem::Deinitialize()
+{
+    if (Importer)
+    {
+        Importer = nullptr;
+    }
+
+    Super::Deinitialize();
+}
---
+++ Source/FragmentsEditor/Private/FragmentsEditorModule.cpp
@@ -0,0 +1,12 @@
+#include "FragmentsEditorModule.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE(FFragmentsEditor, FragmentsEditor);
+
+void FFragmentsEditor::StartupModule()
+{
+}
+
+void FFragmentsEditor::ShutdownModule()
+{
+}
